6cf7e97f00c17f1f2ff99b7c667ec0fc582f4c9b
TBlauwe <tdeblauwe@pm.me>
2023-12-14 18:42:43 +0100

    Added plot generation

--- src/bench_docs/script.py
+++ src/bench_docs/script.py
@@ -1,6 +1,8 @@
 import argparse
 
 import pandas as pd
+from bokeh.models import ColumnDataSource, Legend
+from bokeh.palettes import Category10
 
 from bench_docs.utility import git
 from bench_docs.utility.asserts import assert_file_exists
@@ -8,7 +10,8 @@ from bench_docs.parser.google_benchmark import GoogleBenchmarkParser
 from bench_docs.settings import Settings
 from bench_docs.utility.dsl import retrieve_integer
 from bench_docs.utility.file import retrieve_all_files_from
-
+from bench_docs.utility.units import convert_time
+from bokeh.plotting import figure, show
 
 def run(filepath: str):
     assert_file_exists(filepath)
@@ -37,17 +40,36 @@ def run(filepath: str):
             print(f"Error: {settings.benchmarks_format} is not a supported format.")
 
     # Reorganize dataframe
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    new_icolumns = icolumns.copy()
+    new_icolumns.remove('Datetime')
+    new_icolumns.remove('Git commit/tag')
     df = df.sort_values(by=["Name", "Datetime"])
-    index = pd.MultiIndex.from_frame(df[icolumns])
+    index = pd.MultiIndex.from_frame(df[new_icolumns])
     df.index = index
-    df = df.drop(columns=icolumns)
+    df = df.drop(columns=new_icolumns)
 
     # Generate charts
     for title, chart in settings.charts.items():
         _df = df.query(chart.query)
-        for name, group in _df.groupby(["Name"] + additionnal_columns):
-            print(name)
-            print(group)
+
+        p = figure(title=title, x_axis_type='datetime', toolbar_location="above")
+        groups = _df.groupby(["Name"] + additionnal_columns)
+
+        legends = []
+        i = 0
+
+        for name, group in groups:
+            source = ColumnDataSource(group)
+            line = p.line(x="Datetime", y="Mean", source=source, line_width=2, color=Category10[10][i % 10])
+            legends.append((str(name), [line]))
+            i += 1
+
+        legend = Legend(items=legends, location=(0, -30))
+        legend.location = "top_right"
+        legend.click_policy = "hide"
+        p.add_layout(legend, 'right')
+        show(p)
 
     print("Done")
 
